c53946cd85d9611f73239ffc171b7676e459236e
Kkmilo <kamiilo139698@gmail.com>
2019-12-11 23:50:32 -0500

    comitOne

---
+++ Assets/EnemyIA.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class EnemyIA : MonoBehaviour
+{
+    [SerializeField] private float sightRange= 10;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private GameObject warning= null;
+    [SerializeField] private GameObject detect= null;
+    private Transform enemiTransform;
+
+    void Awake()
+    {
+        enemiTransform= transform;
+    }
+
+    void Update()
+    {
+       DetecPlayer(IsLookinThePlayer(playerTransform.position));
+    }
+    private float timeOnSight;
+    private void DetecPlayer(bool isLookinThePlayer)
+    {
+        if (isLookinThePlayer)
+        {
+            warning.SetActive(true);
+            detect.SetActive(false);
+            if (timeOnSight<=3)
+            {
+                timeOnSight+= Time.deltaTime;
+            }
+            if (!(timeOnSight>=3)) return;
+            warning.SetActive(false);
+            detect.SetActive(true);
+            EditorSceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            if (timeOnSight>0)
+            {
+                timeOnSight-= Time.deltaTime;
+            }
+            if (!(timeOnSight<=0)) return;
+            warning.SetActive(false);
+            detect.SetActive(false);
+        }
+    }
+    private bool IsLookinThePlayer(Vector3 playerPositin)
+    {
+        var displacement= playerPositin- enemiTransform.position;
+        var distanceToPlayer= displacement.magnitude;
+        if (distanceToPlayer<= sightRange)
+        {
+            var dot= Vector3.Dot(enemiTransform.forward,displacement.normalized);
+            if(!(dot >=0.5))return false;
+
+            var layerMask= 1 << 2;
+            layerMask= ~layerMask;
+            if(Physics.Raycast(enemiTransform.position,displacement.normalized,out var hit,sightRange,layerMask))
+            {
+                Debug.DrawRay(enemiTransform.position,displacement.normalized*hit.distance,Color.red);
+                Debug.Log("Did hit");
+                if(!hit.collider.GetComponent<Player>()) return false;
+
+                Debug.DrawRay(enemiTransform.position,displacement.normalized*hit.distance,Color.green);
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float rotar=2;
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * speed;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position -= transform.forward * speed;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up*rotar);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+               transform.Rotate(Vector3.up*-rotar);
+            } 
+    }
+}
