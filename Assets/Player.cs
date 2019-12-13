aeb3c2975eeec600b5c95912749754b88654f77c
KKamilo <kamiilo139698@gmail.com>
2019-12-13 13:15:12 -0500

    fox

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public Animator animator;
+
     public float speed;
-    float rotar=2;
+    float rotar = 2;
     void Update()
     {
-       if (Input.GetKey(KeyCode.W))
-            {
-                transform.position += transform.forward * speed;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.position -= transform.forward * speed;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(Vector3.up*rotar);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-               transform.Rotate(Vector3.up*-rotar);
-            } 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+            animator.SetFloat("Vertical", 1);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+            animator.SetFloat("Vertical", -1);
+
+        }else{
+            animator.SetFloat("Vertical", 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetFloat("Horizontal", 1);
+
+            transform.Rotate(Vector3.up * rotar);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetFloat("Horizontal", -1);
+
+            transform.Rotate(Vector3.up * -rotar);
+        }
+        else
+        {
+            animator.SetFloat("Horizontal", 0);
+
+        }
     }
 }
